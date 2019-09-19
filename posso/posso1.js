b4e8d913c6ca9fd461ade5ea2a63bb73f4a57f58
shibusawasatoshi <shibusawasatoshi@gmail.com>
2019-09-19 11:11:23 +0900

    no message

--- posso/posso1.js
+++ posso/posso1.js
@@ -1,6 +1,10 @@
 const url = "wss://api.sakura.io/ws/v1/62ec74c0-c25d-4085-9836-d49fe7a5432c";
 let kusoFlag = false;
-
+if(navigator.geolocation){
+  console.log("ok");
+}else{
+  console.log("not ok");
+}
 document.querySelector("#keiro").addEventListener('click', ()=>{
   console.log("動け！！");
   let kusoFlag = true;
@@ -12,21 +16,22 @@ function kuso(){
   let kusoFlag = true;
 navigator.geolocation.getCurrentPosition(keiro, keiroero)}*/
 
-function keiro(){
-  var client = new WebSocket(url);
+function keiro(coords){
+
+  let client = new WebSocket(url);
   client.onmessage = function(e) {    
-    var data = JSON.parse(e.data);
+    let data = JSON.parse(e.data);
     if (data.type == 'channels') {
 
-      var datalist = data.payload.channels;
+      let datalist = data.payload.channels;
       
-      for (var i = 0; i < datalist.length; i++) {
+      for (let i = 0; i < datalist.length; i++) {
         if ((datalist[i].channel == 0)||(datalist[i].channel == 1)) {
-          var lat = datalist[0].value;
-          var lng = datalist[1].value;
-     
-  const clat=position.coords.latitude;
-  const clng=position.coords.longitude;
+          let lat = datalist[0].value;
+          let lng = datalist[1].value;
+
+  const clat=coords.latitude;
+  const clng=coords.longitude;
   if (kusoFlag === true) {
     console.log(kusoFlag);
   let kusoFlag = false;
@@ -44,8 +49,8 @@ readstart();
 window.onload=readstart();
 
 function readstart() {
-  var output = document.getElementById('msgarea');
-  var client = new WebSocket(url);
+  let output = document.getElementById('msgarea');
+  let client = new WebSocket(url);
 
   output.innerHTML="接続中です・・・\n";
 
@@ -59,36 +64,36 @@ function readstart() {
   client.onerror = function() {   
     alert("エラーです");
     output.innerHTML="もう一度お試しください";
-  };
+  }
 
 //メッセージを受信した際のイベント
   client.onmessage = function(e) {    
-    var data = JSON.parse(e.data);
+    let data = JSON.parse(e.data);
     if (data.type == 'channels') {
 
-      var datalist = data.payload.channels;
+      let datalist = data.payload.channels;
       
       for (var i = 0; i < datalist.length; i++) {
         if ((datalist[i].channel == 0)||(datalist[i].channel == 1)) {
-          var lat = datalist[0].value;
-          var lng = datalist[1].value;
-          var date = new Date(datalist[i].datetime);
-          var y = date.getFullYear();
-          var m = date.getMonth() + 1;
-          var d = date.getDate();
-          var hour = date.getHours();
-          var min = date.getMinutes();
+          let lat = datalist[0].value;
+          let lng = datalist[1].value;
+          let date = new Date(datalist[i].datetime);
+          let y = date.getFullYear();
+          let m = date.getMonth() + 1;
+          let d = date.getDate();
+          let hour = date.getHours();
+          let min = date.getMinutes();
           
           output.innerHTML =  
             y + "年" + m + "月" + d + "日" + 
             hour + "時" + min + "分" +"のときの位置情報です"+ /*"  https://www.google.com/maps?q="+lat+","+lng+*/"\n";
 
-     var mymap = L.map('mapid').setView([lat , lng], 15);
+     let mymap = L.map('mapid').setView([lat , lng], 15);
      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18
     })
     .addTo(mymap);           
-    var marker = L.marker([lat , lng])
+    let marker = L.marker([lat , lng])
     .addTo(mymap);
         }
       }
